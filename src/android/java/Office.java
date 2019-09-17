1b25cd423f10d5bcd45c536f6fd574fa77918723
Yan Lai <laiguangyan@wondersgroup.com>
2019-09-17 14:55:10 +0800

    init

--- src/android/java/Office.java
+++ src/android/java/Office.java
@@ -36,4 +36,37 @@ public class Office extends CordovaPlugin implements WpsUtil.WpsInterface {
             callbackContext.error("Expected one non-empty string argument.");
         }
     }
+
+    @Override
+    public void doRequest(String filePath) {
+        Log.d("MainActivity", "这里处理你的文件保存事件");
+    }
+
+    @Override
+    public void doFinish() {
+        Log.d("MainActivity", "in doFinish");
+        wpsUtil.appBack();
+        McallbackContext.success(backUrl);
+        // closeWPS("cn.wps.moffice_eng");
+    }
+
+    private void closeWPS(String packageName) {
+        ActivityManager am = (ActivityManager) cordova.getActivity().getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningAppProcessInfo> mRunningProcess = am.getRunningAppProcesses();
+        int pid = -1;
+        //int i = 1;
+        for (ActivityManager.RunningAppProcessInfo amProcess : mRunningProcess) {
+            if (amProcess.processName.equals(packageName)) {
+                pid = amProcess.pid;
+                break;
+            }
+            Log.i("zhuming", "PID: " + amProcess.pid + "(processName=" + amProcess.processName + "UID=" + amProcess.uid + ")");
+
+        }
+        ActivityManager mAm = (ActivityManager) cordova.getActivity().getSystemService(Context.ACTIVITY_SERVICE);
+        //mAm.forceStopPackage("com.bbk.audiofx");
+        //mAm.killBackgroundProcesses(packageName);
+        //android.os.Process.killProcess(pid);
+
+    }
 }
